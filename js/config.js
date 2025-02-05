9ecd7c7766fcd01a2d62cfd2efe91ddb931cd11f
Damar <hehe05092006@gmail.com>
2025-02-05 11:55:31 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'arn:aws:cognito-idp:us-east-1:730342177526:userpool/us-east-1_bmlOntlg0', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '9ditf3b52aq1j33npcn8archpbvj1pdv9sirsrvult2serk6fvm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_bmlOntlg0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7apv17getu5nrnd8rtt563lckv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
